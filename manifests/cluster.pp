318c5d139c433288b54efa4c25d59a93d9af5c6e
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-05-22 13:04:16 +1200

    register with a GPFS cluster

---
+++ manifests/cluster.pp
@@ -0,0 +1,11 @@
+define gpfs.cluster(
+  $server
+){
+
+  require gpfs
+
+  exec{"gpfs_cluster_${name}_${server}":
+    command => "/usr/lpp/mmfs/bin/mmsdrrestore  -p ${server} -R /usr/bin/scp",
+    creates => "/var/mmfs/gen/mmsdrfs"
+  }
+}
