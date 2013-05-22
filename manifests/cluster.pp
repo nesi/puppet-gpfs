95c2217b42c76373531acd6632efce5be3584034
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-05-22 14:16:25 +1200

    ah, it's not an error with the command, just load the host key with sshkey

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -4,8 +4,10 @@ define gpfs::cluster(
 
   require gpfs
 
+  # For this to work, the GPFS server's host key must already be known
+  # try using the sshkey resource
   exec{"gpfs_cluster_${name}_${server}":
-    command => "/usr/lpp/mmfs/bin/mmsdrrestore -p ${server} -R '/usr/bin/scp -oStrictHostKeyChecking=no'",
+    command => "/usr/lpp/mmfs/bin/mmsdrrestore -p ${server} -R /usr/bin/scp",
     creates => "/var/mmfs/gen/mmsdrfs"
   }
 }
